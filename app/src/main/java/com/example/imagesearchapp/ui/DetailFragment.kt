450ce0d62100b7bb1906eb4822938fe985137c45
mortezakdevelop <m.vortex3537007@gmail.com>
2022-03-28 20:05:45 -0700

    create appbar configuration and save state handler in viewmodel

--- app/src/main/java/com/example/imagesearchapp/ui/DetailFragment.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/DetailFragment.kt
@@ -1,24 +1,91 @@
 package com.example.imagesearchapp.ui
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.imagesearchapp.R
 import com.example.imagesearchapp.databinding.FragmentDetailBinding
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_gallery.*
+import kotlinx.android.synthetic.main.unsplash_photo_load_state_footer.*
+import kotlinx.android.synthetic.main.unsplash_photo_load_state_footer.progress_bar
 
 
 class DetailFragment : Fragment() {
 
     lateinit var fragmentDitalBinding: FragmentDetailBinding
+
+    private val args by navArgs<DetailFragmentArgs>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         fragmentDitalBinding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_detail,container,false)
+
+        fragmentDitalBinding.apply {
+            val photo = args.photo
+            Glide.with(this@DetailFragment)
+                .load(photo.urls.full)
+                .listener(object : RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        textViewDescription.isVisible = photo.description != null
+                        return false
+                    }
+
+                })
+                .error(R.drawable.ic_error).into(imageView)
+
+            textViewDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            textViewCreator.apply {
+                text = "Photo by ${photo.user.name} on UnSplash"
+
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+
+
+
         return fragmentDitalBinding.root
     }
 }
--- app/src/main/java/com/example/imagesearchapp/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/example/imagesearchapp/viewmodel/GalleryViewModel.kt
@@ -1,21 +1,20 @@
 package com.example.imagesearchapp.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.example.imagesearchapp.repository.UnSplashRepository
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
-    private val unSplashRepository: UnSplashRepository
+    private val unSplashRepository: UnSplashRepository,
+    @Assisted state:SavedStateHandle
 ):ViewModel() {
 
     //paging
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY,DEFAULT_QUERY)
 
     var photos = currentQuery.switchMap { queryString ->
         unSplashRepository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -25,6 +24,7 @@ class GalleryViewModel @Inject constructor(
         currentQuery.value = query
     }
     companion object{
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "computers"
     }
 }
